193b06ddb2ce822be18bbb32b1e9d1bd0478cba7
Stella <SKraan20@korowa.vic.edu.au>
2018-11-28 16:55:11 +1100

    notsu

--- js/main.js
+++ js/main.js
@@ -9,3 +9,15 @@ $('.highlight').mouseenter(function() {
 $('.highlight').mouseleave(function() {
   $('.annotation').hide();
 })
+
+$(document).ready(function(){
+  $('.annotationone').hide();
+})
+
+$('.highlightone').mouseenter(function() {
+  $('.annotationone').show();
+})
+
+$('.highlightone').mouseleave(function() {
+  $('.annotationone').hide();
+})
