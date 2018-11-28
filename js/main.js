eebddaa688a57e7247f2a62900038130088cdfa4
Stella <SKraan20@korowa.vic.edu.au>
2018-11-28 16:18:33 +1100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,11 @@
 $(document).ready(function(){
-  $('.annotation1').hide();
+  $('.annotation').hide();
 })
 
-$('.highlight1').mouseenter(function() {
-  $('.annotation1').show();
+$('.highlight').mouseenter(function() {
+  $('.annotation').show();
 })
 
-$('.highlight1').mouseleave(function() {
-  $('.annotation1').hide();
-})
-
-$(document).ready(function(){
-  $('.annotation2').hide();
-})
-
-$('.highlight2').mouseenter(function() {
-  $('.annotation2').show();
-})
-
-$('.highlight2').mouseleave(function() {
-  $('.annotation2').hide();
+$('.highlight').mouseleave(function() {
+  $('.annotation').hide();
 })
